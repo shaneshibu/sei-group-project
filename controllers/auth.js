6e51b69e1889f94bcbff14f68b6a9fe5287592f0
Cliff Conolly <cliffconolly@protonmail.com>
2019-07-06 11:47:31 +0100

    updated-login-feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,11 +7,29 @@ function register(req, res, next) {
   User
     .create(req.body)
     .then(user => res.status(201).json({
-      message: `Great, that's done...now your journey begins ${user.username}`
+      message: `Welcome aboard ${user.username}...now your journey begins.`
     }))
     .catch(next)
 }
 
+// Login
+function login(req, res, next) {
+  User
+    .findOne({ email: req.body.email })
+    .then(user => {
+      if (!user || !user.validatePassword(req.body.password)) {
+        throw new Error('Unauthorized')
+      }
+      const token = jwt.sign({ sub: user._id }, secret, { expiresIn: '192h' })
+      res.status(200).json({
+        message: `Hey ${user.usernname}, welcome back.`,
+        token
+      })
+    })
+    .catch(next)
+}
+
 module.exports = {
-  register
+  register,
+  login
 }
