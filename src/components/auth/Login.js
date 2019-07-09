84602cb534b71a40acc3834759e5b1963739fbe8
Cliff Conolly <cliffconolly@protonmail.com>
2019-07-09 16:20:55 +0100

    frontend-login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -10,11 +10,37 @@ class Login extends React.Component {
     return (
       <section className="section">
         <div className="container">
-          <h1 className="title">
-            Login Page
-          </h1>
+          <form onSubmit={this.handleSubmit}>
+            <h2 className="title">Login</h2>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  className={`input ${this.state.error ? 'is-danger' : ''} `}
+                  name="email"
+                  placeholder="Email"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input
+                  className={`input ${this.state.error ? 'is-danger' : ''} `}
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+              </div>
+              {this.state.error && <small className="help is-danger">{this.state.error}</small>}
+            </div>
+            <button type="submit" className="button is-warning">Login</button>
+          </form>
         </div>
       </section>
+
     )
   }
 }
