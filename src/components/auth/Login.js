40858e046fe8535d6c8b920cefdd254377330c27
Cliff Conolly <cliffconolly@protonmail.com>
2019-07-09 21:32:40 +0100

    add-login/auth-files

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import axios from 'axios'
+import Auth from '../../lib/Auth'
 
 class Login extends React.Component {
   constructor() {
     super()
+
+    this.state = { data: {}, error: '' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange({ target: { name, value } }) {
+    const data = { ...this.state.data, [name]: value }
+    this.setState({ data, error: '' })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    axios.post('/api/login', this.state.data)
+      .then(res => {
+        Auth.setToken(res.data.token)
+        this.props.history.push('/users')
+      })
+      .catch(() => this.setState({ error: 'Invalid Crendentials' }))
   }
 
   render() {
@@ -36,7 +57,7 @@ class Login extends React.Component {
               </div>
               {this.state.error && <small className="help is-danger">{this.state.error}</small>}
             </div>
-            <button type="submit" className="button is-warning">Login</button>
+            <button type="submit" className="button is-danger">Login</button>
           </form>
         </div>
       </section>
