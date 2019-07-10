fbb0ed8fc6f73d98a9ddc92197ffe742350297c4
danielagutperl <daniela.gutperl@gmail.com>
2019-07-10 14:58:21 +0100

    adding show place page, minor update to req Destinations component

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import 'bulma'
-
 import './styles.scss'
 
 // import Destinations from './components/Destinations'
@@ -10,13 +9,13 @@ import Navbar from './components/common/Navbar'
 import Home from './components/common/Home'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
+import ShowPlace from './components/places/ShowPlace'
 import UsersIndex from './components/users/UsersIndex'
 import UsersShow from './components/users/UsersShow'
 import UsersTripsIndex from './components/trips/UsersTripsIndex'
 import TripsIndex from './components/trips/TripsIndex'
 // import TripsCreate from './components/trips/TripsCreate'
 import TripsShow from './components/trips/TripsShow'
-import SecureRoute from './components/common/SecureRoute'
 
 const App = () => (
   <BrowserRouter>
@@ -25,8 +24,9 @@ const App = () => (
       <Switch>
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
-        <SecureRoute exact path="/users/:id/trips" component={UsersTripsIndex} />
-        <SecureRoute exact path='/users/:id' component={UsersShow} />
+        <Route path="/places/:placeId" component={ShowPlace} />
+        <Route exact path="/users/:id/trips" component={UsersTripsIndex} />
+        <Route exact path='/users/:id' component={UsersShow} />
         <Route exact path="/users" component={UsersIndex} />
         <Route exact path="/trips" component={TripsIndex} />
         <Route exact path="/trips/:id" component={TripsShow} />
---
+++ src/components/places/ShowPlace.js
@@ -0,0 +1,45 @@
+import React, { Component, Fragment } from 'react'
+import axios from 'axios'
+
+//axios request variables
+const { token, account } = require('../../../config/env')
+const triposoAPI = 'https://www.triposo.com/api/20181213/'
+const endpoint = 'location'
+const format = '.json'
+
+//https://www.triposo.com/api/20181213/location.json?id=London&account=ZU9GVO0H&token=gecvyh2juuwkrp5uif4yicwczfaq4ytd
+
+const paramsBlock = 'id=London'
+
+class ShowPlace extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { place: null }
+  }
+
+  componentDidMount() {
+    this.getPlace()
+  }
+
+  //TODO: figure out how to reuse the destiantions request across multiple pages
+  getPlace() {
+    axios.get(`${triposoAPI}${endpoint}${format}?${paramsBlock}&account=${account}&token=${token}`)
+      .then(res => this.setState({ place: res.data } ))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    if (!this.state.place) return null
+    console.log(this.state)
+    return (
+      <section>
+        <div>
+          {place.name}{place.id}
+        </div>
+      </section>
+    )
+  }
+}
+
+export default ShowPlace
