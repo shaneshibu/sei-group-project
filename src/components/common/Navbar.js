fa848e769a494b50c53c5bbf94b4492e1414af64
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-10 12:11:35 +0100

    navbar options now change when user is logged in or out

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import Auth from '../../lib/Auth'
 
 class Navbar extends React.Component {
@@ -32,9 +32,9 @@ class Navbar extends React.Component {
             {!Auth.isAuthenticated() && <Link className="navbar-item" to="/register">Register</Link>}
             {!Auth.isAuthenticated() && <Link className="navbar-item" to="/login">Login</Link>}
             <Link className="navbar-item" to="/trips">Browse Trips</Link>
-          {Auth.isAuthenticated() && <Link className="navbar-item" to={`/users/${Auth.getUser()}/trips`}>My Trips</Link>}
+            {Auth.isAuthenticated() && <Link className="navbar-item" to={`/users/${Auth.getUser()}/trips`}>My Trips</Link>}
             <Link className="navbar-item" to="/users">People</Link>
-            {Auth.isAuthenticated() && <Link className="navbar-item" to="/users/:id">My Profile</Link>}
+            {Auth.isAuthenticated() && <Link className="navbar-item" to={`/users/${Auth.getUser()}`}>My Profile</Link>}
             {Auth.isAuthenticated() && <a className="navbar-item" onClick={this.logout}>Logout</a>}
           </div>
         </div>
@@ -44,4 +44,4 @@ class Navbar extends React.Component {
 }
 
 
-export default Navbar
+export default withRouter(Navbar)
