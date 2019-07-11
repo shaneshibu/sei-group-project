8825493c5119932fd9072d32c109dfc79cdcb85b
danielagutperl <daniela.gutperl@gmail.com>
2019-07-11 17:56:09 +0100

    Yay! working search from input to click and show page

--- src/components/places/Destinations.js
+++ src/components/places/Destinations.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import axios from 'axios'
 
 const { token, account } = require('../../../config/env')
@@ -16,21 +17,36 @@ class Destinations extends Component {
     this.handleClick = this.handleClick.bind(this)
   }
 
-  componentDidMount() {
-    this.getDestinations()
-  }
-
-  getDestinations() {
-    if (this.props.searchCriteria.searchType === 'Place') {
-      axios.get(`${triposoAPI}location.json?id=${this.props.searchCriteria.inputValue}&fields=all&account=${account}&token=${token}`)
-        .then( res => this.setState({ destinations: res.data.results }))
-        .catch(err => console.log(err))
-    } else {
-      axios.get(`${triposoAPI}poi.json?location_id=London$annotate=trigram:${this.props.searchCriteria.inputValue}&fields=id,name,score,snippet,location_id,tag_labels&order_by=-score&account=${account}&token=${token}`)
-        .then( res => this.setState({ destinations: res.data.results }))
-        .catch(err => console.log(err))
-    }
-  }
+  // componentDidMount() {
+  //   this.getDestinations()
+  //   console.log('heya fr cDM in destionations')
+  //   console.log(`${triposoAPI}location.json?id=${this.props.searchCriteria.inputValue.Place}&fields=all&account=${account}&token=${token}`)
+  //
+  //   // console.log(axios.get(`${triposoAPI}poi.json?location_id=London$annotate=trigram:${this.props.searchCriteria.inputValue}&fields=id,name,score,snippet,location_id,tag_labels&order_by=-score&account=${account}&token=${token}`)
+  // }
+  //
+  // componentDidUpdate(prevProps) {
+  //   console.log('prev', prevProps)
+  //   console.log('props', this.props)
+  //   // if (prevProps.inputValue && prevProps.inputValue.Place === this.props.inputValue.Place) {
+  //   console.log('hey fr component did update')
+  //   console.log(`${triposoAPI}location.json?id=${this.props.searchCriteria.inputValue.Place}&fields=all&account=${account}&token=${token}`)
+  //   // this.getDestinations()
+  //   // }
+  // }
+  //
+  // getDestinations() {
+  //   console.log('trying to get destinations')
+  //   if (this.props.searchCriteria.searchType === 'Place') {
+  //     axios.get(`${triposoAPI}location.json?id=${this.props.searchCriteria.inputValue.Place}&fields=all&account=${account}&token=${token}`)
+  //       .then( res => this.setState({ destinations: res.data.results }))
+  //       .catch(err => console.log(err))
+  //   } else {
+  //     axios.get(`${triposoAPI}poi.json?location_id=${this.props.searchCriteria.inputValue.Place}&annotate=trigram:${this.props.searchCriteria.inputValue.POI}&fields=id,name,score,snippet,location_id,tag_labels&order_by=-score&account=${account}&token=${token}`)
+  //       .then( res => this.setState({ destinations: res.data.results }))
+  //       .catch(err => console.log(err))
+  //   }
+  // }
 
   handleClick( e ) {
     // e.persist() - what does this do??
@@ -39,18 +55,17 @@ class Destinations extends Component {
   }
 
   render() {
-    const { destinations } = this.state
+    const { destinations } = this.props
     if (!destinations) return null
     return (
       <div>
         <ul>
           {
-            this.state.destinations.map(destination => (
+            destinations.map(destination => (
               <li
                 key={destination.id}
                 onClick={() => this.handleClick(destination.id)}>
                 {destination.name}{destination.id}
-                {this.props.searchCriteria.inputValue}
               </li>
             ))
           }
@@ -59,4 +74,7 @@ class Destinations extends Component {
     )
   }
 }
-export default Destinations
+export default withRouter(Destinations)
+
+
+// {this.props.searchCriteria.inputValue}
