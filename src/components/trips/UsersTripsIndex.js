d70847aca31470c06b6348e5b73e12816fe3faf5
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-11 12:22:16 +0100

    users can now create new trips

--- src/components/trips/UsersTripsIndex.js
+++ src/components/trips/UsersTripsIndex.js
@@ -8,7 +8,9 @@ class UsersTripsIndex extends React.Component {
 
     this.state = {}
     this.getUsersTrips = this.getUsersTrips.bind(this)
-    this.handleClick = this.handleClick.bind(this)
+    //this.handleClick = this.handleClick.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
@@ -20,12 +22,30 @@ class UsersTripsIndex extends React.Component {
       .then(res => this.setState({ trips: res.data }))
   }
 
-  handleClick({ target }) {
-    console.log(target.dataset.tripid)
+  // handleClick({ target }) {
+  //   console.log(target.dataset.tripid)
+  // }
+
+  handleChange({ target }) {
+    this.setState({ newTripInput: target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    console.log(this.props)
+    if (this.state.newTripInput && this.state.newTripInput.length) {
+      axios.post(`/api/users/${this.props.match.params.id}/trips`, {
+        title: this.state.newTripInput,
+        user_id: this.props.match.params.id
+      })
+        .then(() => this.getUsersTrips())
+        .catch(err => console.log(err))
+    }
   }
 
   render() {
-    console.log(this.state.trips)
+    console.log(this.state)
     if (!this.state.trips) return null
     return (
       <section className="section">
@@ -33,6 +53,25 @@ class UsersTripsIndex extends React.Component {
           <h1 className="title">
             My Trips
           </h1>
+          <div>
+            <form className="field has-addons" onSubmit={this.handleSubmit}>
+              <div className="control">
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Create a New Trip"
+                  value={this.state.newTripInput || ''}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="control">
+                <button className="button">Create</button>
+              </div>
+            </form>
+
+
+
+          </div>
           {this.state.trips && this.state.trips.map(trip => (
             <div key={trip._id} data-tripid={trip._id}>
               <Link to={`/trips/${trip._id}`}>
