498742ed15fc520c08fd56260ec2bdc34950adeb
Cliff Conolly <cliffconolly@protonmail.com>
2019-07-11 23:25:18 +0100

    css upgrades

--- src/components/users/UsersShow.js
+++ src/components/users/UsersShow.js
@@ -17,7 +17,6 @@ class UsersShow extends React.Component {
     axios.get(`/api/users/${this.props.match.params.id}`)
       .then(res => this.setState({ user: res.data }))
       .catch(err => console.log(err))
-
   }
 
   getUserInfo(){
@@ -35,8 +34,6 @@ class UsersShow extends React.Component {
           <div className="location">{user.locationHome}</div>
         </div>
       </div>
-
-
     )
   }
 
@@ -47,7 +44,7 @@ class UsersShow extends React.Component {
       <section className="section page-container">
         <div className="container">
           <h1 className="title">
-            Users Show Page
+          
           </h1>
           {userInfo}
         </div>
@@ -57,93 +54,3 @@ class UsersShow extends React.Component {
 }
 
 export default UsersShow
-
-
-
-
-// import React from 'react'
-// import axios from 'axios'
-//
-// class UsersShow extends React.Component {
-//   constructor() {
-//     super()
-//
-//     this.state = {}
-//     this.getUserData = this.getUserData.bind(this)
-//   }
-//
-//   componentDidMount() {
-//     this.getUserData()
-//   }
-//
-//   getUserData() {
-//     axios.get(`/api/users/${this.props.match.params.id}`)
-//       .then(res => this.setState({ user: res.data }))
-//       .catch(err => console.log(err))
-//
-//   }
-//
-//   render() {
-//     console.log(this.state)
-//     return (
-//       <section className="section">
-//         <div className="container">
-//           <h1 className="title">
-//             Users Show Page
-//           </h1>
-//           <div className="columns is-mobile is-multiline">
-//              (
-//             <div key={user._id} className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
-//
-//               <div className="card">
-//                 <div className="card-header">
-//                   <h4 className="card-header-title">{trip.title}</h4>
-//                 </div>
-//                 <div className="card-content">
-//                   {user.places && user.places((place, i) => (
-//
-//                     <div key={i}>
-//                       <p>{user.name}</p>
-//                       <p>{user.userName}</p>
-//                       <p>{user.email}</p>
-//                       <p>{user.image}</p>
-//                       <p>{user.locationHome}</p>
-//
-//                       <div className="card-image">
-//                         <figure className="image is-4by3">
-//                           <img src={place.thumbnail} alt="Placeholder image" />
-//                         </figure>
-//                       </div>
-//                     </div>
-//
-//
-//                   ))}
-//                 </div >
-//
-//                 <div className="card-image">
-//                   <figure className="image">
-//                     <img src={trip.image} alt={trip.name}/>
-//                   </figure>
-//                 </div>
-//                 <div className="card-content">
-//                   <h5 className="title is-6">{trip.description}</h5>
-//                   <h6 className="subtitle is-6">{trip.title.places}</h6>
-//                 </div>
-//               </div>
-//
-//             </div>
-//             ))}
-//           </div>
-//
-//
-//
-//
-//
-//         </div>
-//       </section>
-//     )
-//   }
-// }
-//
-// export default UsersShow
-//
