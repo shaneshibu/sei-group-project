de504c8c7dabb07a0453c6087b91ffadebc9d373
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-11 15:21:31 +0100

    makes axios get request

--- src/components/users/UsersShow.js
+++ src/components/users/UsersShow.js
@@ -4,9 +4,24 @@ import axios from 'axios'
 class UsersShow extends React.Component {
   constructor() {
     super()
+
+    this.state = {}
+    this.getUserData = this.getUserData.bind(this)
+  }
+
+  componentDidMount() {
+    this.getUserData()
+  }
+
+  getUserData() {
+    axios.get(`/api/users/${this.props.match.params.id}`)
+      .then(res => this.setState({ user: res.data }))
+      .catch(err => console.log(err))
+
   }
 
   render() {
+    console.log(this.state)
     return (
       <section className="section">
         <div className="container">
