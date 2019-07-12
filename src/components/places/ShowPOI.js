78972ba406bf9f4b01d46ed44e336b472ed5408c
danielagutperl <daniela.gutperl@gmail.com>
2019-07-12 13:16:53 +0100

    showing info, address, directions, price, train, bus on showPOI

--- src/components/places/ShowPOI.js
+++ src/components/places/ShowPOI.js
@@ -1,41 +1,44 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-//axios request variables
 const { token, account } = require('../../../config/env')
 const triposoAPI = 'https://www.triposo.com/api/20181213/'
 const endpoint = 'property'
 const format = '.json'
 
-
-const paramsBlock = 'poi_id=W__3107461'
+// https://www.triposo.com/api/20181213/poi.json?annotate=trigram:eureka&trigram=%3E=0.3&count=10&fields=id,name,score,snippet,location_id,tag_labels&order_by=-score&account=ZU9GVO0H&token=2eys99ozaguzd3csvch3tekb8c2jkvuv
 
 class ShowPOI extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { place: null }
+    this.state = { poi: null }
   }
 
   componentDidMount() {
-    this.getPlace()
+    this.getPOI()
   }
 
   //TODO: figure out how to reuse the destiantions request across multiple pages
-  getPlace() {
-    axios.get(`${triposoAPI}${endpoint}${format}?${paramsBlock}&account=${account}&token=${token}`)
-      .then(res => this.setState({ place: res.data } ))
+  getPOI() {
+    axios.get(`https://www.triposo.com/api/20181213/property.json?poi_id=${this.props.match.params.placeId}&account=ZU9GVO0H&token=2eys99ozaguzd3csvch3tekb8c2jkvuv`)
+    // axios.get(`${triposoAPI}poi.json?$annotate=trigram:eureka&trigram=%3E=0.3&count=10&fields=id,name,score,snippet,location_id,tag_labels&order_by=-score&account=${account}&token=${token}`)
+      .then(res => this.setState({ poi: res.data } ))
       .catch(err => console.log(err))
   }
 
   render() {
-    const { place } = this.state
-    if (!place) return null
-    console.log(this.state)
+    if (!this.state.poi) return null
+    console.log('this state', this.state)
+    console.log('info for render', this.state)
     return (
       <section>
         <div>
-          {place.name}{place.id}
+          <p>{this.state.poi.results[2].key}: {this.state.poi.results[2].value}</p> //address
+          <p>{this.state.poi.results[3].key}: {this.state.poi.results[3].value}</p> //directions
+          <p>{this.state.poi.results[5].key}: {this.state.poi.results[5].value}</p> //price
+          <p>{this.state.poi.results[8].key}: {this.state.poi.results[8].value}</p> //train
+          <p>{this.state.poi.results[7].key}: {this.state.poi.results[7].value}</p> //bus
         </div>
       </section>
     )
